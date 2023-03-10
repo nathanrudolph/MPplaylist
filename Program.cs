af383fdb35a85924f081453c4f35b8be9e4d1861
n8rei <n8rei@NOOT-SFF>
2023-03-09 16:30:34 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+// See https://aka.ms/new-console-template for more information
+using MountainProjectAPI;
+namespace MPplaylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nightcrawlerURL = "https://www.mountainproject.com/route/105920684/the-nightcrawler";
+            
+            MPWrapper mpWrapper = new MPWrapper();
+            mpWrapper.AddUrl(nightcrawlerURL);
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            mpWrapper.GetRoutesFromURLsAsync();
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed);
+            List<Route> routes = mpWrapper.Routes;
+            
+            foreach (var route in routes)
+            {
+                Console.WriteLine(route.Name);
+            }
+            Console.WriteLine("Complete.");
+        }
+    }
+}
